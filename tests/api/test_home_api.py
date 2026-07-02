2fc52d3e5e1f7c7f7d0fd6b61a323c007572db43
sapellysaivivek <saiviveksapelly@gmail.com>
2026-07-02 22:23:06 +0530

    changing rerun

--- tests/api/test_home_api.py
+++ tests/api/test_home_api.py
@@ -69,7 +69,7 @@ def test_delete_note_api(token):
 @allure.feature("NOTES MANAGEMENT API")
 @allure.story("api response time within 2s")
 @allure.description("This test verifies that the response time for the /api/notes/ endpoint is within 2 seconds.")
-@pytest.mark.api
+@pytest.mark.api.flaky(reruns=3, reruns_delay=5)
 def test_notes_api_response_time(token):
     start_time = time.time()
     response = get("https://practice.expandtesting.com/notes/api/notes/", token)
